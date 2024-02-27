0e9e5a283b42c948497b301ddc8bff701f697ae9
sebjvidal <seb.vidal@btinternet.com>
2024-02-27 10:58:42 +0000

    Initial Commit

---
+++ Adaptive-UICollectionView-Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Adaptive-UICollectionView-Demo
+//
+//  Created by Seb Vidal on 27/02/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
